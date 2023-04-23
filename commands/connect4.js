ba338c100900b55f770d60f878aa79ee329803fb
Jacob385 <122482836+Jacob385@users.noreply.github.com>
2023-04-23 22:05:28 +0000

    added different instances of the board. NEEDS heavy testing

--- commands/connect4.js
+++ commands/connect4.js
@@ -2,22 +2,6 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SlashCommand
 
 var gameHolder = [];
 
-function game(player1, player2) {
-  this.player1 = player1,
-    this.player2 = player2,
-    currentPlayer = 1,//witch player starts
-
-    //[X][]is row [][X]is column
-    board = [
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0]];
-
-}
-
 
 module.exports = {
   data:
@@ -38,26 +22,46 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.deferReply();
-    {
-    let player1 = interaction.user.id;// message.author.id TODO wich one?
-    let player2 = interaction.options.getUser('opponent').id;
 
-    //checks if user already has a game
     let currentGame = null;
-    for (let x = 0; x < gameHolder.length; x++) {
-      if (gameHolder[x].player1 === player1 || gameHolder[x].player2 === player2) {
-        //TODO user has a game already
-        currentGame = gameHolder[x];
-        x = gameHolder.length;//break
+    {
+      let player1 = interaction.user.id;// message.author.id TODO wich one?
+      let player2 = interaction.options.getUser('opponent').id;
+
+      //checks if user already has a game
+      for (let x = 0; x < gameHolder.length; x++) {
+        if (gameHolder[x].player1 === player1 || gameHolder[x].player2 === player1) {
+          //TODO user has a game already
+          currentGame = gameHolder[x];
+          x = gameHolder.length;//break
+          console.log("game already exist");///TODO remove debug
+        }
+      }
+      if (currentGame === null) {
+        //adds new game to holder
+        var game = {
+          player1: player1,
+          player2: player2,
+          currentPlayer: 1,//witch player starts
+
+          //[X][]is row [][X]is column
+          board: [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]]
+        }
+
+        gameHolder[gameHolder.length] = game
+        currentGame = game;
+        console.log("game added");///TODO remove debug
+
       }
     }
-    if (currentGame === null) {
-      //adds new game to holder
-      gameHolder[gameHolder.length] = new game(player1, player2);
-    }
-    }
 
-    boardBuilder = function(lastMove) {
+    boardBuilder = function(lastMove, board) {
       var boardString = '';
 
       for (let x = 0; x < board[0].length; x++)
@@ -85,22 +89,22 @@ module.exports = {
       return boardString + ':one: :two: :three: :four: :five: :six: :seven:';
     }
 
-    isColumnFull = function(columnNum) { return board[0][columnNum] != 0; }
+    isColumnFull = function(columnNum, board) { return board[0][columnNum] != 0; }
 
-    placePiece = function(columnNum) {
-      for (let x = board.length - 1; x >= 0; x--) {
-        if (board[x][columnNum] == 0) {
-          board[x][columnNum] = currentPlayer;
-          currentPlayer = 3 - currentPlayer;//swaps 1 for 2 and 2 for 1
-          return checkWin(x, columnNum);
+    placePiece = function(columnNum, game) {
+      for (let x = game.board.length - 1; x >= 0; x--) {
+        if (game.board[x][columnNum] == 0) {
+          game.board[x][columnNum] = game.currentPlayer;
+          game.currentPlayer = 3 - game.currentPlayer;//swaps 1 for 2 and 2 for 1
+          return checkWin(x, columnNum, game);
         }
       }
     }
 
-    checkWin = (x, y) => {
+    checkWin = (x, y, game) => {
       x = Number(x);
       y = Number(y);
-      let color = board[x][y];
+      let color = game.board[x][y];
       let inARow = [];
       //[direction][x/y value]
       let vectorArray = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1]];
@@ -114,16 +118,15 @@ module.exports = {
           currentCell.y += vectorArray[direction][1];
 
           try {
-            if (board[currentCell.x][currentCell.y] === color)
+            if (game.board[currentCell.x][currentCell.y] === color)
               inARow[direction]++;
             else
               step = 3;
           } catch (error) { step = 3; }
         }
       }
-      console.log(inARow);
+
       for (let test = 0; test < 4; test++) {
-        console.log(inARow[test] + ", " + (test === 3 ? 0 : inARow[test + 4]));
         if (inARow[test] + (test === 3 ? 0 : inARow[test + 4]) >= 3) {
           return color;
         }
@@ -170,45 +173,79 @@ module.exports = {
     );
 
     const embed = new EmbedBuilder()
-      .setColor(0x0099FF)
+      .setColor((currentGame.currentPlayer === 1?0x0099FF:0xFF0000))
       .setTitle('Connect 4')
-      .setDescription(boardBuilder(-1));
+      .setDescription(boardBuilder(-1, currentGame.board));
 
 
 
-    await interaction.editReply({ content: `You challenged <@${player2}>.`, embeds: [embed], components: [row, row2] });
+    await interaction.editReply({ content: `You challenged <@${currentGame.player2}>.`, embeds: [embed], components: [row, row2] });
 
     {//buttons 
-      const wait = require('node:timers/promises').setTimeout;
-
-
-
+      
       const filter = i => {
         var columnIndex = Number(i.customId.split("n")[1]) - 1;
-        ['column1', 'column2', 'column3', 'column4', 'column5', 'column6', 'column7'].some(buttonID => i.customId === buttonID)
-          && !isColumnFull(columnIndex)
-          && i.user.id === (currentPlayer === 1 ? player1 : player2);
+
+        for (let x = 0; x < gameHolder.length; x++) {
+          if (gameHolder[x].player1 === i.user.id || gameHolder[x].player2 === i.user.id) {
+
+            console.log("\nfound game :" + x);///TODO remove debug
+
+            //if a user hits quit
+            if(i.customId === 'column-1' && (i.user.id === gameHolder[x].player1 || i.user.id === gameHolder[x].player2))
+            return true;
+            
+             return ['column1', 'column2', 'column3', 'column4', 'column5', 'column6', 'column7'].some(buttonID => i.customId === buttonID)
+                && !isColumnFull(columnIndex, gameHolder[x].board)           
+              && i.user.id === (gameHolder[x].currentPlayer === 1 ? gameHolder[x].player1 : gameHolder[x].player2);
+                 
+          }
+        }
+        return false;
       };
 
 
       const collector = interaction.channel.createMessageComponentCollector({ filter, max: 42 });
       collector.on('collect', async i => {
+        console.log("passed filter");///TODO remove debug
         await i.deferUpdate();
 
-        if (columnIndex < 0 || placePiece(columnIndex) > 0) {
+        console.log("inside collector");///TODO remove debug
+       
+        var columnIndex = Number(i.customId.split("n")[1]) - 1;
+        var currentGame;
+        let x;
+        for (x = 0; x < gameHolder.length; x++) {
+          if (gameHolder[x].player1 === i.user.id || gameHolder[x].player2 === i.user.id) {
+            currentGame = gameHolder[x];
+            break;
+          }
+        }
+        
+
+
+        if (columnIndex < 0) {
+          console.log("quit game");///TODO remove debug
           collector.stop();
-          await i.editReply({ content: ':trophy: ' + (currentPlayer === 2 ? `<@${player1}>.` : `<@${player2}>.`) + " Wins! :trophy:", embeds: [embed.setDescription(boardBuilder(columnIndex))], components: [] });
+gameHolder.splice(x,1);
+          await i.editReply({ content: ':trophy: ' + (currentGame.currentPlayer === 2 ? `<@${currentGame.player1}>.` : `<@${currentGame.player2}>.`) + " Wins! :trophy:", embeds: [embed], components: [] });
+
+        } else if (placePiece(columnIndex, currentGame) > 0) {
+          console.log("game won");///TODO remove debug
+          collector.stop();
+          gameHolder.splice(x,1);
+          await i.editReply({ content: ':trophy: ' + (currentGame.currentPlayer === 2 ? `<@${currentGame.player1}>.` : `<@${currentGame.player2}>.`) + " Wins! :trophy:", embeds: [embed.setDescription(boardBuilder(columnIndex, currentGame.board))], components: [] });
         }
         else {
-
+          console.log("place piece");///TODO remove debug
           //locks buttons on full rows
-          (columnIndex <= 5 ? row : row2).components.at(columnIndex % 5).setDisabled(isColumnFull(columnIndex));
+          (columnIndex <= 5 ? row : row2).components.at(columnIndex % 5).setDisabled(isColumnFull(columnIndex, currentGame.board));
 
-          if (currentPlayer == 1)
+          if (currentGame.currentPlayer == 1)
             embed.setColor(0x0099FF);
           else
             embed.setColor(0xFF0000);
-          await i.editReply({ content: '', embeds: [embed.setDescription(boardBuilder(columnIndex))], components: [row, row2] });
+          await i.editReply({ content: '', embeds: [embed.setDescription(boardBuilder(columnIndex, currentGame.board))], components: [row, row2] });
         }
 
       });
