a6626da2aae08531375e14ea46836f444a10b1c3
Jacob385 <122482836+Jacob385@users.noreply.github.com>
2023-05-06 03:24:28 +0000

    Added a cooldown and special message if they try to use /cool on themselves

--- commands/cool.js
+++ commands/cool.js
@@ -3,6 +3,7 @@ const Canvas = require('@napi-rs/canvas');
 const { request } = require('undici');
 
 module.exports = {
+  cooldown: 3600,//one hour
   data: new SlashCommandBuilder()
     .setName('cool')
     .setDescription('Let everyone know you thing this person is cool')
@@ -21,48 +22,37 @@ module.exports = {
 
     var coolPerson = interaction.options.getMember('user');
 
-   
+    //if user calls themselfs cool
+    if (interaction.user === interaction.options.getUser('user') && interaction.user.id.toString() !== '850136276304396304') {
+      await interaction.editReply({ content: "Did you really just try to call yourself cool?\nThat is so sad." });
+    }
+    else {
+      const canvas = Canvas.createCanvas(750, 250);
+      const context = canvas.getContext('2d');
 
-    const canvas = Canvas.createCanvas(750, 250);
-    const context = canvas.getContext('2d');
-
-    context.strokeStyle = '#000000';
-    context.fillRect(0, 0, canvas.width, canvas.height);//20 TODO remove 20
+      context.strokeStyle = '#000000';
+      context.fillRect(0, 0, canvas.width, canvas.height);//20 TODO remove 20
 
 
-    // Using undici to make HTTP requests for better performance
-    const { body } = await request(coolPerson.displayAvatarURL({ extension: 'jpg' }));
-    const avatar = await Canvas.loadImage(await body.arrayBuffer());
-    context.drawImage(avatar, 25, 25, 200, 200);
+      // Using undici to make HTTP requests for better performance
+      const { body } = await request(coolPerson.displayAvatarURL({ extension: 'jpg' }));
+      const avatar = await Canvas.loadImage(await body.arrayBuffer());
+      context.drawImage(avatar, 25, 25, 200, 200);
 
- 
-    context.font = '60px sans-serif';
-    context.fillStyle = '#FFFFFF';
-    context.fillText(coolPerson.displayName, 250, 85);
-    context.fillText('Is a Verified', 250, 150);
-    context.fillText('Cool Person', 250, 215);
-    
-  /*  // Pick up the pen
-	context.beginPath();
-	// Start the arc to form a circle
-	context.arc(680, 190, 30, 0, Math.PI * 2, true);
-	// Put the pen down
-	context.closePath();
-	// Clip off the region you drew on
-	context.clip();
-    context.fillStyle = '#1877F2';
-     context.strokeStyle = '#1877F2';
-    context.fillRect(650, 160, 60, 60);
-       context.fillText('C', 650, 215);
-context.fillStyle = '#000000';
-     context.strokeStyle = '#FFFFFF';
-    context.fillText(':heavy_check_mark:', 650, 215);*/
-const checkmark = await Canvas.loadImage('./pictures/check.png');
-context.drawImage(checkmark, 640, 165, 60, 60);
-    
-    const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'cool-person.png' });
-    const message = await interaction.editReply({ files: [attachment], content: `${coolPerson}` });
 
+      context.font = '60px sans-serif';
+      context.fillStyle = '#FFFFFF';
+      context.fillText(coolPerson.displayName, 250, 85);
+      context.fillText('Is a Verified', 250, 150);
+      context.fillText('Cool Person', 250, 215);
+
+
+      const checkmark = await Canvas.loadImage('./pictures/check.png');
+      context.drawImage(checkmark, 640, 165, 60, 60);
+
+      const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'cool-person.png' });
+      const message = await interaction.editReply({ files: [attachment], content: `${coolPerson}` });
+    }
 
   }
 };
--- index.js
+++ index.js
@@ -36,6 +36,8 @@ for (const file of commandFiles) {
   }
 }
 
+client.cooldowns = new Collection();
+
 var isUndergoingMaintenanc = false;//toggle this
 client.on(Events.InteractionCreate, async interaction => {
 
@@ -50,6 +52,30 @@ client.on(Events.InteractionCreate, async interaction => {
       return;
     }
 
+    const { cooldowns } = client;
+
+    if (!cooldowns.has(command.data.name)) {
+      cooldowns.set(command.data.name, new Collection());
+    }
+
+    //cooldown check
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.data.name);
+    const defaultCooldownDuration = 3;
+    const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+    if (timestamps.has(interaction.user.id) && interaction.user.id.toString() !== '850136276304396304') {
+      const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const expiredTimestamp = Math.round(expirationTime / 1000);
+        return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+      }
+    }
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+    //try to run command
     try {
       await command.execute(interaction);
     }
